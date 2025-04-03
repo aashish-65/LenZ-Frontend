f28fb549e7778b838ac881a7bd1e92e1cfb9505b
aashish-65 <aashishprasad65@gmail.com>
2025-04-04 01:21:06 +0530

    Turn on the service for test

--- src/utils/serviceAvailability.js
+++ src/utils/serviceAvailability.js
@@ -1,11 +1,11 @@
 // utils/serviceAvailability.js
 export const isServiceUnavailable = () => {
-  const currentTime = new Date();
-  const istOffset = 5.5 * 60 * 60 * 1000;
-  const istTime = new Date(currentTime.getTime() + istOffset);
+  // const currentTime = new Date();
+  // const istOffset = 5.5 * 60 * 60 * 1000;
+  // const istTime = new Date(currentTime.getTime() + istOffset);
 
-  const hours = istTime.getUTCHours();
+  // const hours = istTime.getUTCHours();
 
-  return hours >= 20 || hours < 10;
-  // return false
+  // return hours >= 20 || hours < 10;
+  return false
 };
